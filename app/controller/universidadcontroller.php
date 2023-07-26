2ad4f891de3affafe54d8c9e4870d0f9e2e0b2db
Dsm-Montalvo <al222210568@gmail.com>
2023-07-25 22:32:13 -0600

    Prueba de Conexion

---
+++ app/controller/universidadcontroller.php
@@ -0,0 +1,10 @@
+<?php
+include("./../../database/Conexion.php");
+
+/* Consultas de selección que devuelven un conjunto de resultados */
+if ($resultado = $conn->query("SELECT * FROM  carreras LIMIT 10")) {
+    printf("La selección devolvió %d filas.\n", $resultado->num_rows);
+
+    /* liberar el conjunto de resultados */
+    $resultado->close();
+}

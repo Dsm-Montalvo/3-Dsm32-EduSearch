e60c7f52783e36e7b06d3177054f9453931d0d7e
Dsm-Montalvo <al222210568@gmail.com>
2023-08-04 03:58:53 -0600

    Prueba de conexion base de datos

--- app/controller/universidadcontroller.php
+++ app/controller/universidadcontroller.php
@@ -2,7 +2,7 @@
 include("./../../database/Conexion.php");
 
 /* Consultas de selección que devuelven un conjunto de resultados */
-if ($resultado = $conn->query("SELECT * FROM  carreras LIMIT 10")) {
+if ($resultado = $conn->query("SELECT * FROM  carrera LIMIT 10")) {
     printf("La selección devolvió %d filas.\n", $resultado->num_rows);
 
     /* liberar el conjunto de resultados */
